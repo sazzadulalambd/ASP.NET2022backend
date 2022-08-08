142e986890393a9fdfcefd84eefe0a58afe07701
sazzad ul alam Shawon <93247296+sazzadulalambd@users.noreply.github.com>
2022-08-08 19:21:14 +0600

    4 layer ARC

---
+++ AppLayer_BePartner/Entity/AdminModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class AdminModel
+    {
+        public string Ad_Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public System.DateTime Dob { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Security_key { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Nid { get; set; }
+
+        public string Password { get; set; }
+
+        public string Img { get; set; }
+
+    }
+}
